cc421ff87102352ac2fde1015f4643c48a813ef5
Flavio Leggio <flavioleggio@gmail.com>
2017-05-02 23:14:11 +0200

    Aggiunto tool per testare il controllo dei motori

---
+++ tools/motors_test.cpp
@@ -0,0 +1,34 @@
+#include <gazebo/transport/transport.hh>
+#include <gazebo/msgs/msgs.hh>
+
+#include "MotorsCommand.pb.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace gazebo;
+
+int main(int argc, char** argv)
+{
+  msgs::MotorsCommand msg;
+  msg.set_left_torque(0.1);
+  msg.set_right_torque(0.1);
+
+  transport::init();
+  transport::run();
+  transport::NodePtr node(new transport::Node());
+  node->Init("default");
+
+  transport::PublisherPtr pub = node->Advertise<msgs::MotorsCommand>("~/motors_control");
+  pub->WaitForConnection();
+
+  while(1)
+  {
+    pub->Publish(msg);
+    printf("msg published\n");
+    common::Time::MSleep(100);
+  }
+
+  transport::fini();
+  return 0;
+}
