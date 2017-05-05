6ad348be2b9a8f382daee92f144482e7df869c7c
Flavio Leggio <flavioleggio@gmail.com>
2017-05-05 17:33:38 +0200

    Fix runsim.sh (parametrizzato sulla working directory del repository)

--- runsim.sh
+++ runsim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source /usr/share/gazebo/setup.sh
 
-export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:/home/flavio/Scrivania/Workspace/rgsim/worlds
-export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:/home/flavio/Scrivania/Workspace/rgsim/models
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/home/flavio/Scrivania/Workspace/rgsim/build/models/robot
+export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:${PWD}/worlds
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${PWD}/models
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:${PWD}/build/models/robot
 
 gazebo worlds/rgsim.world
